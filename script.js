301cd47ba4086c6f5dcdbf066c39b6fb58e2bfbf
MBoddah <72345270+MBoddah@users.noreply.github.com>
2020-10-07 20:20:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+
+let chat = document.querySelector('.chat');
+let oldMessageElement;
+let key = '0';
+
+while (localStorage.getItem(key) != null)
+{
+    oldMessageElement = document.createElement('p');
+    oldMessageElement.classList.add('mymessages');
+    oldMessageElement.innerHTML = localStorage.getItem(key);
+    chat.appendChild(oldMessageElement);
+    key = String(Number(key) + 1);
+}
+
 function send()
 {
     let message = document.querySelector('.mess_area');
-    let chat = document.querySelector('.chat');
-
-//    localStorage.setItem('0', message);
+    localStorage.setItem(key, message.value);
     let messageElement = document.createElement('p');
-
-    messageElement.innerHTML = message.value;
-    chat.appendChild(messageElement);
     messageElement.classList.add('mymessages');
     messageElement.innerHTML = message.value;
     chat.appendChild(messageElement);
+    key = String(Number(key) + 1);
     message.value = '';
 }
-
